edd466ffa75b4842e2703120806f1c64a56a6a6e
Charles Edeki Oghenemineh <charlesedeki093@gmail.com>
2023-10-28 20:38:43 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Charles Edeki",
+  title: "Web Developer & Data Analyst",
+  email: "charlesedeki093@gmail.com",
+  gitHub: "CharlesEdeki",
+  instagram: "charlesedeki",
+  linkedIn: "Charles Edeki",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "_Oghenemineh_",
 };
 
 const primaryColor = "#4E567E";
