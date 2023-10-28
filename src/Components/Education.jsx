330dca2ae3a83754b227ef72a030ee4f9158570b
Charles Edeki Oghenemineh <charlesedeki093@gmail.com>
2023-10-28 22:19:43 +0000

    Updated portfolio site, colours, and added a new section for education.

---
+++ src/Components/Education.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Education = () => {
+  return (
+    <section className="padding" id="education">
+      <div
+        style={{
+          backgroundColor: "white",
+          width: "50%",
+          padding: "4rem",
+          margin: "1rem auto",
+          textAlign: "center",
+        }}
+      >
+        <h2>Education</h2>
+        <p>
+          <strong>Federal University of Technology, Minna</strong>
+          <br />
+          B.Tech. Chemistry
+          <br />
+          2019 - 2025
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Education;
